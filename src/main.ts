cc4da5a596f4ab9cbcee84e86c061068d0a37104
Janeen Taitingfong <jatai@Janeens-MacBook-Air.local>
2025-09-25 12:05:51 -0700

    step 1: getting it to print out any data

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!");;
+console.log("Welcome to Lemonade Stand!!");
+dayReport(5, 1, 59);
+
+class LemonadeStand{
+	currentAssets: number
+}
+
+class manager{
+	currentDay: number;
+	currentWeather: number;
+	currentPrice: number;
+}
+
+function dayReport(currentDay: number, currentWeather: number, currentPrice: number){
+	console.log(currentDay + "\n");
+	console.log(currentWeather + "\n");
+	console.log(currentPrice + "\n");
+}
